598d5f159f777f0f07f716842005fc040719e20d
Jack Baumann <wavejbaumann@gmail.com>
2017-05-25 11:25:19 -0500

    3rd AWS Upload
    Added image carousel to the about page

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
+
+  //Carousel initiation and timer
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+  // var timerVar = setInterval(timer, 3000);
+  // function timer() {
+  //     $('.carousel').carousel('next');
+  // }
 });
