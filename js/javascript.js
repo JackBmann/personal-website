445f07d02444a7d7e1ba13d6cdacf6a4d24b566f
Jack Baumann <wavejbaumann@gmail.com>
2017-08-24 10:42:00 -0500

    Finished Home page

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
+  $('.parallax').parallax();
 
   //Carousel initiation and timer
   $('.carousel.carousel-slider').carousel({fullWidth: true});
